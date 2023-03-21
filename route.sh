255c1bec84b312641b04603f27810c2165bd56c9
root <vm kathara>
2023-03-21 09:08:52 +0100

    initial commit

---
+++ route.sh
@@ -0,0 +1,5 @@
+ip route add 172.16.80.0/20 via 172.17.0.2
+ip route add 172.16.64.0/20 via 172.17.0.2
+ip route add 172.16.96.0/20 via 172.17.0.2
+ip route add 172.16.112.0/20 via 172.17.0.2
+
---
+++ routeDell.sh
@@ -0,0 +1,4 @@
+ip route del 172.16.80.0/20 via 172.17.0.2
+ip route del 172.16.64.0/20 via 172.17.0.2
+ip route del 172.16.96.0/20 via 172.17.0.2
+ip route del 172.16.112.0/20 via 172.17.0.2
