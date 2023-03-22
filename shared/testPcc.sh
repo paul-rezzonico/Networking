3e2bc1a1066d2b1d86a4773df571f82be0a93a9a
Paul Rezzonico <paul.rezzo@gmail.com>
2023-03-22 23:05:48 +0100

    add test for pcc

--- shared/testPcc.sh
+++ shared/testPcc.sh
@@ -1,47 +1,47 @@
-#Ce fichier de test permet de tester les iptables et les règles de pare-feu mise en place sur 
-#la zone employée.
+#Ce fichier de test permet de tester les iptables et les règles de pare-feu mise en place sur la zone
+#administration.
 
-#Test 1 on vérifie que le ping est possible entre les deux machines pca et pcb :
-echo "Test 1 : ping entre pca et pcb"
-ping -c 4 -w 5 172.16.64.2
+#Test 1 on vérifie que le ping est possible entre les deux machines pcc et pcd :
+echo "Test 1 : ping entre pcc et pcd"
+ping -c 4 -w 5 172.16.96.2
 if [ $? -eq 0 ]
 then
 echo -e "\e[32mTest 1 : OK\e[0m"
 else
-echo -e "\e[31mTest Failed !\e[0m"
+echo -e "\e[31mTest 1 : Test failed !\e[0m"
 return 1
 fi
 
-#Test 2 on vérifie que le ping est impossible entre les deux machines pca et pcc :
-echo "Test 2 : ping entre pca et pcc"
-ping -c 4 -w 5 172.16.96.1
+#Test 2 on vérifie que le ping est impossible entre les deux machines pcc et pca :
+echo "Test 2 : ping entre pcc et pca"
+ping -c 4 -w 5 172.16.64.1
 if [ $? -eq 1 ]
 then
 echo -e "\e[32mTest 2 : OK\e[0m"
 else
-echo -e "\e[31mTest Failed !\e[0m"
+echo -e "\e[31mTest 2 : Test failed !\e[0m"
 return 1
 fi
 
-#Test 3 on vérifie que le ping est impossible entre pca et internet :
-echo "Test 3 : ping entre pca et internet"
+#Test 3 on vérifie que le ping est impossible entre pcc et internet :
+echo "Test 3 : ping entre pcc et internet"
 ping -c 4 -w 5 google.com
 if [ $? -eq 1 ]
 then
 echo -e "\e[32mTest 3 : OK\e[0m"
 else
-echo -e "\e[31mTest Failed !\e[0m"
+echo -e "\e[31mTest 3 : Test failed !\e[0m"
 return 1
 fi
 
-#Test 4 on vérifie qu'internet est accessible depuis pca :
-echo "Test 4 : internet accessible par pca"
+#Test 4 on vérifie qu'internet est accessible depuis pcc :
+echo "Test 4 : internet accessible par pcc"
 apt-get update
 if [ $? -eq 0 ]
 then
 echo -e "\e[32mTest 4 : OK\e[0m"
 else
-echo -e "\e[31mTest Failed !\e[0m"
+echo -e "\e[31mTest 4 : Test failed !\e[0m"
 return 1
 fi
 
@@ -178,6 +178,3 @@ else
 echo -e "\e[31mTest Failed !(2/2)\e[0m"
 return 1
 fi
-
-
-
