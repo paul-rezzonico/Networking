d63b2d014c8d1ed625f5cb2d0ae5ca0eabcc47e2
Paul Rezzonico <paul.rezzo@gmail.com>
2023-03-22 11:46:15 +0100

    correct

--- shared/testPca.sh
+++ shared/testPca.sh
@@ -46,8 +46,8 @@ return 1
 fi
 
 #Test 5 on vérifie que l'on ne peut pas accéder aux ports 25,53,80,465,587,2525 depuis pca :
-echo "Test 5 : port 25,53,80,465,587,2525 filtrés (non accessibles)ssss"
-nmap -p 22,443,25,53,80,465,587,2525  172.16.112.35 | grep closed
+echo "Test 5 : port 25,53,80,465,587,2525 filtrés (non accessibles)"
+nmap -p 25,53,80,465,587,2525  172.16.112.35 | grep closed
 if [ $? -eq 1 ]
 then
 echo -e "\e[32mTest 5 : OK\e[0m"
