b90f5ef1c879c16b657f483e807b65d6ac8e250a
Paul Rezzonico <paul.rezzo@gmail.com>
2023-03-22 15:33:19 +0100

    refacto

--- shared/testPca.sh
+++ shared/testPca.sh
@@ -55,10 +55,26 @@ else
 echo -e "\e[31mTest Failed !\e[0m"
 return 1
 fi
+nping --tcp -p 25 | grep closed >> logs.txt
+if [ $? -eq 1 ]
+then
+echo -e "\e[32mTest 5 : OK\e[0m"
+else
+echo -e "\e[31mTest Failed !\e[0m"
+return 1
+fi
 
 #Test 6 on vérifie que l'on ne peut pas accéder au port 53 depuis pca :
 echo "Test 6 : port 53 filtré (non accessibles)"
 nmap -p 53 172.16.112.35 | grep closed >> logs.txt
+if [ $? -eq 1 ]
+then
+echo -e "\e[32mTest 6 : OK\e[0m"
+else
+echo -e "\e[31mTest Failed !\e[0m"
+return 1
+fi
+nping --tcp -p 53 | grep closed >> logs.txt
 if [ $? -eq 1 ]
 then
 echo -e "\e[32mTest 6 : OK\e[0m"
@@ -77,10 +93,26 @@ else
 echo -e "\e[31mTest Failed !\e[0m"
 return 1
 fi
+nping --tcp -p 80 | grep closed >> logs.txt
+if [ $? -eq 1 ]
+then
+echo -e "\e[32mTest 7 : OK\e[0m"
+else
+echo -e "\e[31mTest Failed !\e[0m"
+return 1
+fi
 
 #Test 8 on vérifie que l'on ne peut pas accéder au port 465 depuis pca :
 echo "Test 8 : port 465 filtré (non accessibles)"
 nmap -p 465 172.16.112.35 | grep closed >> logs.txt
+if [ $? -eq 1 ]
+then
+echo -e "\e[32mTest 8 : OK\e[0m"
+else
+echo -e "\e[31mTest Failed !\e[0m"
+return 1
+fi
+nping --tcp -p 465 | grep closed >> logs.txt
 if [ $? -eq 1 ]
 then
 echo -e "\e[32mTest 8 : OK\e[0m"
@@ -94,15 +126,32 @@ echo "Test 9 : port 587 filtré (non accessibles)"
 nmap -p 587 172.16.112.35 | grep closed >> logs.txt
 if [ $? -eq 1 ]
 then
-echo -e "\e[32mTest 9 : OK\e[0m"
+echo -e "\e[32mTest 9 : OK (1/2)\e[0m"
 else
-echo -e "\e[31mTest Failed !\e[0m"
+echo -e "\e[31mTest Failed !(1/2)\e[0m"
 return 1
 fi
+nping --tcp -p 587 | grep closed >> logs.txt
+if [ $? -eq 1 ]
+then
+echo -e "\e[32mTest 9 : OK (2/2)\e[0m"
+else
+echo -e "\e[31mTest Failed !(2/2)\e[0m"
+return 1
+fi
+
 
 #Test 10 on vérifie que l'on ne peut pas accéder au port 2525 depuis pca :
 echo "Test 10 : port 2525 filtré (non accessibles)"
 nmap -p 2525 172.16.112.35 | grep closed >> logs.txt
+if [ $? -eq 1 ]
+then
+echo -e "\e[32mTest 10 : OK\e[0m"
+else
+echo -e "\e[31mTest Failed !\e[0m"
+return 1
+fi
+nping --tcp -p 2525 | grep closed >> logs.txt
 if [ $? -eq 1 ]
 then
 echo -e "\e[32mTest 10 : OK\e[0m"
@@ -121,26 +170,14 @@ else
 echo -e "\e[31mTest Failed !\e[0m"
 return 1
 fi
-
-#Test 7 on vérifie que l'on peut accéder au port 443 depuis pca :
-nping -p 443 --tcp 172.16.112.35
-if [ $? -eq 0 ]
-then
-echo -e "\e[32mTest 7 : OK\e[0m"
-else
-echo -e "\e[31mTest Failed !\e[0m"
-return 1
-fi
-
-#Test 8 on vérifie que l'on ne peut pas accéder au port 25,53,80,465,587,2525 :
-echo "Test 8 : port 25,53,80,465,587,2525 filtrés (non accessibles)"
-nping -p 25,53,80,465,587,2525 172.16.112.35
+nping --tcp -p 443 | grep filtered >>logs.txt
 if [ $? -eq 1 ]
 then
-echo -e "\e[32mTest 8 : OK\e[0m"
+echo -e "\e[32mTest 6 : OK\e[0m"
 else
 echo -e "\e[31mTest Failed !\e[0m"
 return 1
 fi
+
 
 
