e5c961336ccc818a6b4f9f235a15c0b53f0e831d
Paul Rezzonico <paul.rezzo@gmail.com>
2023-03-24 16:12:22 +0100

    remove sudo from script

--- shared/offline.sh
+++ shared/offline.sh
@@ -1,8 +1,4 @@
 iptables -F FORWARD
-
-iptables -P INPUT DROP
-iptables -P OUTPUT DROP
-iptables -A INPUT -i lo -j ACCEPT
 iptables -P FORWARD DROP
 
 iptables -A FORWARD -p tcp -m multiport --sport 25,53,80,465,587,2525 -s 172.16.96.0/20 -d 172.16.112.0/20 -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
